b488e882c43ae9e3288f2ad10f1c1f4c063797fe
BayzU <bayzedullah@gmail.com>
2023-04-03 09:32:40 -0400

    Fixed default remaining cal

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -13,7 +13,7 @@ const remainingCalories = document.getElementById('remaining-calories')
 
 goodMorningUser.textContent = userFirstName;
 calorieGoalSpanTag.textContent = userCalorieGoal;
-remainingCalories.textContent = parseInt(JSON.parse(localStorage.remaining || "0"));
+remainingCalories.textContent = parseInt(JSON.parse(localStorage.remaining || userCalorieGoal));
 
 const exercisesDBOption = {
   method: "GET",
